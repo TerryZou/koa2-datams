d444739912211179ec9e1017141e80b659244c63
wangzhuo <wangzhuo@ihealthlabs.com.cn>
2017-10-19 16:41:37 +0800

    11
    11

--- routes/datams.js
+++ routes/datams.js
@@ -1,9 +1,20 @@
 const router = require('koa-router')()
 
 router.get('/device_act_user', async (ctx, next) => {
-  await ctx.render('device_act_user', {
-    title: 'Hello Koa 2!'
-  })
+  try{
+    if(ctx.session.user != 'null' && ctx.session.user == 'line-height'){
+      await ctx.render('device_act_user', {
+        title: 'Hello Koa 2!'
+      })
+    }else {
+      await ctx.render('login', {
+        title: 'login!'
+      })
+    }
+  }
+  catch(ex){
+    console.log(ex.message);
+  }
 })
 
 module.exports = router
